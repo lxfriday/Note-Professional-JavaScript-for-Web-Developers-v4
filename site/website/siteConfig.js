b06401c86b381a731485014c66b7ae86be663a81
lxfriday <liu3248184446@outlook.com>
2019-12-31 11:00:36 +0800

    fix: project name

--- site/website/siteConfig.js
+++ site/website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "/Note-Professional-JavaScript-for-Web-Developers-v4/",
+  projectName: "",
   organizationName: "facebook",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
