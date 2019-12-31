70f1409374e59c2124ab8e5eddac40be221ae2ed
lxfriday <liu3248184446@outlook.com>
2019-12-31 10:54:34 +0800

    fix: add baseurl

--- site/website/siteConfig.js
+++ site/website/siteConfig.js
@@ -21,16 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
+  cname: "js-professional.lxfriday.xyz",
   title: "Test Site", // Title for your website.
   tagline: "A website for testing",
   url: "https://your-docusaurus-test-site.com", // Your website URL
-  baseUrl: "/", // Base URL for your project */
+  baseUrl: "/Note-Professional-JavaScript-for-Web-Developers-v4/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "",
+  projectName: "/Note-Professional-JavaScript-for-Web-Developers-v4/",
   organizationName: "facebook",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
